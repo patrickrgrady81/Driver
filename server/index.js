7c950858620d5dc8d12782b3aa0a2df93a568ff5
Patrick Grady <patrickrgrady81@gmail.com>
2022-05-18 07:34:06 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server Started on port ${PORT}`);
+})
